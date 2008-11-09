1961e7bfac3a21b4b4d8ada60db555ea6c793bd4
Terence Lee <hone02@gmail.com>
2008-11-08 20:37:51 -0500

    * in README, corrected the file to run * added LICENSE information to setup.rb * changed layout of app, text input is now at the top.  scrollbar now works. * added refresh button and capability * fixed posting status * posting now refreshes the app

--- tshoe.rb
+++ tshoe.rb
@@ -29,7 +29,11 @@ class Tshoe < Shoes
   TWITTER_PASSWORD = ''
   TWITTER_USERNAME = ''
   LIMIT = 140
+  STATUS_STACK_HEIGHT = 70
+  STATUS_RIGHT_PANE_WIDTH = 50
+  TEXT_BOX_WIDTH = 235
   BACKGROUND = "#2F2F2F"
+  DEFAULT_TEXT = "what are you doing?"
   Default_png = <<PNGSTART
 \211PNG\r\n\032\n\000\000\000\rIHDR\000\000\0000\000\000\0000\004\003\000\000\000\245,\344\264\000\000\000\001sRGB\000\256\316\034\351\000\000\0000PLTE\207cC\207eJ\211iS\212qb\214vn\214~}\220\207\215\217\220\235\224\233\260\227\244\301\233\257\324\237\267\342\241\277\360\242\304\373\236\311\377\245\313\3731j\275\350\000\000\000\001bKGD\000\210\005\035H\000\000\000\tpHYs\000\000\v\023\000\000\v\023\001\000\232\234\030\000\000\000\atIME\a\330\003\034\003\0172\"\337U\003\000\000\001TIDAT8\313c\020\304\001\030F%\bK000`\225`\016\357H\026\300&\221\375\356\335\333\311\214\230\022\032\357V\244\317\371\035\210!!\320w]\200\201u\377f\210\004\243\022\\\232\345\334\004\240l\314\017F\220\004s\347\256\251\214P\t\211?\016@\222\363\215!PB \372\317\236wMP\t\255\237 \222\365n\002PB\370\334d\345\234\347\020-\002\266?A\f\341{\r@\t\311_\n\002\354@\275`\340\367\002,q~\002P\302\372;\243 \363\271\004\210D\034X\253\320~\240\204\200\317SFA\241\365\r\020\t\333\037p\035\002~_A\022\023 \0226`;D\336\001\355\020\360\275\212\244C\353\227\"\220d{\vt\225\200\3157F\240\313\n \022\354o\003\200\244\324kE\220\253\200Ng{\023\000\r\333}\213\030\005\005j\237\203|\316z\267H\320\373\a\324\353\002\271\257\234\030\325\317O\002\205\225@\355\253\316s\213\301\022\300x\020;\177\262s\337KCp \262\256\271\263]QP\331\330\330\330\304PP\300\363\354\333\323I\320\210\022v\026\020\024\210{\367\356\335\373\211@]\246\251\216\360\250\005\231c\321\001\004\211 \363\030\030Q\342\\\200\001\232\nF\363\a\035$\000|\224\234q\3363+s\000\000\000\000IEND\256B`\202
 PNGSTART
@@ -50,8 +54,7 @@ PNGSTART
     end
     debug( "State: #{@state.inspect}" )
     @page = 1
-    fetch_data
-    listen
+    refresh
   end
 
   def fetch_data
@@ -118,8 +121,9 @@ PNGSTART
 
   # send message and clear the text box
   def upandaway
-    @twitter_config.post_message(@i_say.text)
+    @twitter_config.twitter_client.status( :post, @i_say.text )
     @i_say.text = ''
+    refresh
   end
 
   # Method responsable for parsing each incoming message from the XML answer
@@ -140,31 +144,37 @@ PNGSTART
   # GUI area definition method. This method draws the incoming messages area, and the message insertion area.
   def listen
     background BACKGROUND
-    # stack :width => 1.0, :height => -50 do # SHOES TICKET submitted for negative heights inside a page.
-    stack :width => 1.0, :height => 500 do 
+    # text entry
+    flow :width => 1.0, :margin_right => gutter, :height => STATUS_STACK_HEIGHT do
       background BACKGROUND
-      get_thread
-    end
-    flow :width => 1.0, :height => 50 do
-      background BACKGROUND
-      stack :width => -50 do
-        @i_say = edit_box "what are you doing ?", :margin => 5, :width => 250, :height => 50, :size => 9 do
+      stack :width => -STATUS_RIGHT_PANE_WIDTH, height => STATUS_STACK_HEIGHT do
+        @i_say = edit_box DEFAULT_TEXT, :margin => 5, :width => TEXT_BOX_WIDTH, :height => STATUS_STACK_HEIGHT, :size => 9 do
           @remaining.replace string_alert
           if @i_say.text[-1] == ?\n
             upandaway
           end
         end
       end
-      stack :width => 50 do
-        para(link(" Update ", :size => 8, :font => "Arial", :fill => "#4992E6" , :stroke => "#D5E0ED", :underline => false) { upandaway })
-        para(link("<· ", :size => 8, :font => "Arial", :stroke => "#3276BA", :underline => false) { alert("previous page (temporarily disabled by Twitter)") },
+      stack :width => STATUS_RIGHT_PANE_WIDTH, height => STATUS_STACK_HEIGHT do
+        para( link( " Update ", :size => 8, :font => "Arial", :fill => "#4992E6" , :stroke => "#D5E0ED", :underline => false ) { upandaway } )
+        para( link( "<· ", :size => 8, :font => "Arial", :stroke => "#3276BA", :underline => false) { alert("previous page (temporarily disabled by Twitter)") },
              "  1  ", 
              link(" ·>", :size => 8, :font => "Arial", :stroke => "#3276BA", :underline => false) { alert("next page (temporarily disabled by Twitter)") }, :top => 15, :size => 7, :font => "Arial", :stroke => "#3276BA", :underline => false)
         @remaining = para "140", " chars", :top => 30, :size => 6, :font => "Arial", :stroke => "#3276BA"
+        para( link( " Refresh ", :size => 8, :font => "Arial", :fill => "#4992E6" , :stroke => "#D5E0ED", :underline => false ) { refresh } )
       end
     end
+
+    # displays timeline for friends
+    get_thread
   end
 
+  # refresh messages
+  def refresh
+    self.clear
+    fetch_data
+    listen
+  end
 end
 
 Shoes.app :title => "Twitter needs Shoes", :width => 300, :height => 550, :radius => 12, :resizable => true
